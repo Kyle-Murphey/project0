7db199bbc97208e47ac21de59f23ee36ef63eece
Kyle Murphey <kyle.r.murphey@ou.edu>
2019-09-10 11:06:42 -0600

    fixed array format and added var to get string length

--- project0.c
+++ project0.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 void process_input(char input[])
 {
-  char [] underTen = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-  char [] underTwenty = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-  char [] multiplesOfTen = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-  char [] bigNumbers = {"hundred", "thousand", "million"};
+  char * underTen[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  char * underTwenty[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+  char * multiplesOfTen[] = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+  char * bigNumbers[] = {"hundred", "thousand", "million"};
+
+  
 }
 
 int main(int argc, char** argv)
 {
   char buffer[10];
+  int length;
   fgets(buffer, 10, stdin);
+  length = strlen(buffer) - 1;
+  printf("Length: %d\n", length);
   printf("Got: %s\n", buffer);
 }
