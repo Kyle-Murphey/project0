e2223495bd44beaae471b92608658dffa3c49d1b
Kyle Murphey <kyle.r.murphey@ou.edu>
2019-09-10 12:32:40 -0600

    added some basic functionality to process_input; filters out leading 0's

--- project0.c
+++ project0.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-void process_input(char input[])
+void process_input(char input[], int length)
 {
   char * underTen[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
   char * underTwenty[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
   char * multiplesOfTen[] = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
   char * bigNumbers[] = {"hundred", "thousand", "million"};
 
-  
+  int number;
+  int flag = 0;
+  int actualLength = length;
+  for (int i = 0; i < length; ++i)
+    {
+      number = input[i] - '0';
+      if (number > 0)
+	{
+	  flag = 1;
+	}
+      if (number == 0 && flag == 0)
+	{
+	  --actualLength;
+	  continue;
+	}
+      else if (actualLength == 1)
+	printf("output: %s\n", underTen[number - 1]);
+    }
+  if (length == 1)
+    {
+      //int number = input[0] - '0';
+      //printf("output: %s\n", underTen[number - 1]);
+    }
 }
 
 int main(int argc, char** argv)
@@ -17,6 +39,15 @@ int main(int argc, char** argv)
   int length;
   fgets(buffer, 10, stdin);
   length = strlen(buffer) - 1;
+
+  if (length == 1)
+    {
+      int a = buffer[0] - '0';
+      printf("item: %d\n", a);
+    }
+  
   printf("Length: %d\n", length);
   printf("Got: %s\n", buffer);
+
+  process_input(buffer, length);
 }
