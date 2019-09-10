7df5bc3c48c74e075b9da7105bec27b7a3ac5e6d
Kyle Murphey <kyle.r.murphey@ou.edu>
2019-09-10 08:58:59 -0600

    added number arrays

--- project0.c
+++ project0.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void process_input(char input[])
+{
+  char [] underTen = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  char [] underTwenty = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+  char [] multiplesOfTen = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+  char [] bigNumbers = {"hundred", "thousand", "million"};
+}
+
 int main(int argc, char** argv)
 {
   char buffer[10];
